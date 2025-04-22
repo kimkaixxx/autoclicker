8a466414c27bac3d6748dcf04d4e40368a94f4b1
Donghyun Kim <lxraxx1@gmail.com>
2025-04-22 02:23:31 -0400

    Initial Commit

---
+++ autoclicker/autoclickerApp.swift
@@ -0,0 +1,17 @@
+//
+//  autoclickerApp.swift
+//  autoclicker
+//
+//  Created by Donghyun Kim on 4/22/25.
+//
+
+import SwiftUI
+
+@main
+struct autoclickerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ autoclickerTests/autoclickerTests.swift
@@ -0,0 +1,17 @@
+//
+//  autoclickerTests.swift
+//  autoclickerTests
+//
+//  Created by Donghyun Kim on 4/22/25.
+//
+
+import Testing
+@testable import autoclicker
+
+struct autoclickerTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
